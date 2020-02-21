609b650b5e34b5843f5fe6d386475cfb3e420009
Robert Penrod <robertpenrod37@gmail.com>
2020-02-21 00:04:23 -0500

    Polished end of level buttons and changed background color

---
+++ Assets/ButtonAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour
+{
+    public float offsetAmount;
+
+    private Vector3 initialPosition;
+    private Vector2 initialEffectDistance;
+
+    private void Start()
+    {
+        initialPosition = GetComponent<RectTransform>().position;
+        initialEffectDistance = GetComponent<Shadow>().effectDistance;
+    }
+
+    public void DepressButton()
+    {
+        GetComponent<RectTransform>().position = initialPosition + new Vector3(offsetAmount, -offsetAmount);
+        GetComponent<Shadow>().effectDistance = new Vector2(0, 0);
+    }
+
+    public void RestoreButton()
+    {
+        GetComponent<RectTransform>().position = initialPosition;
+        GetComponent<Shadow>().effectDistance = initialEffectDistance;
+    }
+}
